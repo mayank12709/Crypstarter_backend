45b7349b78c7497d59a8d8c1dcc3cf80926a7eca
mayank12709 <mayank.srivastav@oodles.io>
2021-12-13 12:48:23 +0530

    Add CORS origin

--- src/main/java/io/oodles/wallet/configuration/WalletConfiguration.java
+++ src/main/java/io/oodles/wallet/configuration/WalletConfiguration.java
@@ -4,11 +4,19 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WalletConfiguration {
+public class WalletConfiguration implements WebMvcConfigurer {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder){
         return  builder.build();
+    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200", "http://wallet.crypstarter.network/")
+                .allowedMethods("GET", "PUT", "POST", "DELETE");
     }
 }
