3b6a5b1a2ed187ecc7758cbcc6eff0e4d4a88479
mayank12709 <mayank.srivastav@oodles.io>
2021-12-13 13:22:38 +0530

    Add CORS origin

--- src/main/java/io/oodles/wallet/configuration/WalletConfiguration.java
+++ src/main/java/io/oodles/wallet/configuration/WalletConfiguration.java
@@ -8,15 +8,16 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WalletConfiguration implements WebMvcConfigurer {
+public class WalletConfiguration{
+//        implements WebMvcConfigurer {
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder){
         return  builder.build();
     }
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "http://wallet.crypstarter.network/")
-                .allowedMethods("GET", "PUT", "POST", "DELETE");
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:4200", "http://wallet.crypstarter.network/")
+//                .allowedMethods("GET", "PUT", "POST", "DELETE");
+//    }
 }
--- src/main/java/io/oodles/wallet/controller/AccountController.java
+++ src/main/java/io/oodles/wallet/controller/AccountController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
+@CrossOrigin(value = "*")
 @RestController
 @RequestMapping("/common/api/v1")
 public class AccountController {
