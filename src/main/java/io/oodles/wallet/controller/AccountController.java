cf0610087ca882c003aac49d66f72a194c387ff6
mayank12709 <mayank.srivastav@oodles.io>
2021-12-13 14:37:21 +0530

    modify cors logic

--- src/main/java/io/oodles/wallet/controller/AccountController.java
+++ src/main/java/io/oodles/wallet/controller/AccountController.java
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
-@CrossOrigin(value = "*")
 @RestController
 @RequestMapping("/common/api/v1")
+@CrossOrigin(origins = "*")
 public class AccountController {
 
     @Autowired
